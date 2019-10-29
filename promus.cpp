b2603f203d267a69bae3b9f16e3db8b87984d83b
sriram gn <38837878+Sriramgn0@users.noreply.github.com>
2019-10-29 18:37:25 +0530

    Update promus.cpp
    time complexity decreased

--- promus.cpp
+++ promus.cpp
@@ -7,13 +7,9 @@ int main()
     int number,i, value;
     cout<<"Enter Number = ";
     cin>>number;
-    value=0;
-    for (i=1;i<=number;i++){
-        if (number%i==0) //Condition to find number who while it modulo with i the value is 0 (zero)
-        value = value +i;
-        }
-        //Conditions to determining prime number or not
-        if(value==(number+1)){
+    
+        if((value==2)||(value==3)||((value+1)%6==0)||((value-1)%6==0))
+        {
             cout<<"PRIMES";
         }
         else{
